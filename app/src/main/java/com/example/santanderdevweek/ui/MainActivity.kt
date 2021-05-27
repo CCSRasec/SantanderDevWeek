f087ca076a322b155b42ff78858d297896c70189
Chrystian Cesar <chrystian.cesar@luby.software>
2021-05-27 13:56:30 -0400

    projeto finalizado

---
+++ app/src/main/java/com/example/santanderdevweek/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.santanderdevweek.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.santanderdevweek.R
+import com.example.santanderdevweek.data.Conta
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        buscarContaCliente()
+
+    }
+}
